c2fcfe190e7a7645bd855a3ba010e3472a192741
Adi Putra <adiputrapermana@gmail.com>
2022-07-30 14:38:01 +0700

    initialize

---
+++ src/Acme/Product/AcmeProductModule.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\Product;
+
+
+use Pandawa\Component\Module\AbstractModule;
+
+/**
+ * @author Adi Putra <adiputrapermana@gmail.com>
+ */
+class AcmeProductModule extends AbstractModule
+{
+
+}
