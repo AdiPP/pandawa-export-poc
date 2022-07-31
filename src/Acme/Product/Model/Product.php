1b546fb59786ada74bd0fe8143fd4a6d80585c06
Adi Putra <adiputrapermana@gmail.com>
2022-07-31 12:19:18 +0700

    feat(product): add product model and table

---
+++ src/Acme/Product/Model/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\Product\Model;
+
+
+use Pandawa\Component\Ddd\AbstractModel;
+
+/**
+ * @author Adi Putra <adiputrapermana@gmail.com>
+ */
+class Product extends AbstractModel
+{
+
+}
---
+++ src/Acme/Product/Repository/ProductRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\Product\Repository;
+
+
+use Pandawa\Component\Ddd\Repository\Repository;
+
+/**
+ * @author Adi Putra <adiputrapermana@gmail.com>
+ */
+class ProductRepository extends Repository
+{
+
+}
